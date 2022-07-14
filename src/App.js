4ae3475fc8f7386d59cd4710a8708fe653d11b9b
jackgeorge11 <jackkgeorgee@gmail.com>
2022-07-15 00:53:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="home">
+      <div className="left">
+        <h1 className="thick">Tess Buckley</h1>
+        <h3>Thinker, singer, creator.</h3>
+        <p className="--muted">© 2022 by Tess Buckley</p>
+      </div>
+      <nav className="right">
+        <div className="wrapper">
+          <span className="flow ai-ethics" />
+          <button id="ai-ethics">
+            <h1 className="thick">Ethics of AI</h1>
+          </button>
+        </div>
+        <div className="wrapper">
+          <span className="flow" />
+          <button>
+            <h1 className="thick">About</h1>
+          </button>
+        </div>
+
+        <div className="wrapper">
+          <span className="flow publications" />
+          <button id="publications">
+            <h1 className="thick">Publications</h1>
+          </button>
+        </div>
+        <div className="wrapper">
+          <span className="flow" />
+          <button>
+            <h1 className="thick">Contact</h1>
+          </button>
+        </div>
+        <div className="wrapper">
+          <span className="flow music" />
+          <button id="music">
+            <h1 className="thick">Music</h1>
+          </button>
+        </div>
+      </nav>
+    </main>
   );
 }
 
